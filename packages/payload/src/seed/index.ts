4cfa921e8fefc081ebdce131f914b853e0e8d970
Jan Beck <jancbeck@users.noreply.github.com>
2024-09-02 16:15:21 +0200

    remove @payload-config alias from payload package

--- packages/payload/src/seed/index.ts
+++ packages/payload/src/seed/index.ts
@@ -1,9 +1,10 @@
 import { getPayload } from "payload";
-import config from "@payload-config";
+
+import config from "../payload.config";
 import { seedUsers } from "./collections/users";
 
 async function run() {
-  const payload = await getPayload({config});
+  const payload = await getPayload({ config });
 
   try {
     const isSeeded =
